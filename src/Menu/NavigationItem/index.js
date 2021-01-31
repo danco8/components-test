def114e8679c5f0d283372abe87765c1bddf3e35
unknown <danco24000@gmail.com>
2021-01-31 22:32:17 +0200

    fixed menu

--- src/Menu/NavigationItem/index.js
+++ src/Menu/NavigationItem/index.js
@@ -7,12 +7,15 @@ const NavigationItem = (props) => {
     return (
         <div className={styles.root}>
             {props.active ? <img className={styles.activeSign} src={'/images/activeLine.svg'} alt=' ' /> : null}
-            <div className={styles.menuLine}>
-                <div className={styles.IconWrapper}><Icon src={props.text} /></div>
-                <p className={styles.Text}>{props.text} </p>
-                <Icon src='ActionIcon' />
+            <div className={styles.container}>
+
+                <div className={styles.menuLine}>
+                    <div className={styles.IconWrapper}><Icon src={props.text} /></div>
+                    <p className={styles.Text}>{props.text} </p>
+                    <Icon src='ActionIcon' />
+                </div>
+                <Divider />
             </div>
-            <Divider />
         </div>
     );
 };
--- src/Search/AdvanceSearch/Option/index.js
+++ src/Search/AdvanceSearch/Option/index.js
@@ -4,10 +4,10 @@ import styles from "./Option.module.scss";
 const Option = (props) => {
     return (
         <div className={styles.root}>
-            <div className={styles.menuLine}>
+            <div className={styles.line}>
                 <img src={`/images/${props.color}.svg`} alt=' ' />
                 <p className={styles.Text}>{props.text} </p>
-                <img className={styles.arrow} src={'/images/smallRight.svg'} alt=' '/>
+                <img className={styles.arrow} src={'/images/smallRight.svg'} alt=' ' />
             </div>
 
         </div>
